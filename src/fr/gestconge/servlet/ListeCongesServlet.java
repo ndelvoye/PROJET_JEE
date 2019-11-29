ec7affaefa771f32d0e2eaea2b758da3279691ad
ndelvoye <nicolhas.delvoye@gmail.com>
2019-11-29 01:22:41 +0100

    ListeConges - Filtrage par rapport à l'équipe si pas RH

--- src/fr/gestconge/servlet/ListeCongesServlet.java
+++ src/fr/gestconge/servlet/ListeCongesServlet.java
@@ -3,6 +3,7 @@ package fr.gestconge.servlet;
 import fr.gestconge.bean.Demande;
 import fr.gestconge.bean.Employe;
 import fr.gestconge.dao.DemandeDAO;
+import fr.gestconge.dao.EmployeDAO;
 import fr.gestconge.utils.Vues;
 
 import javax.servlet.ServletException;
@@ -12,6 +13,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -20,11 +22,26 @@ public class ListeCongesServlet extends HttpServlet {
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         HttpSession session = request.getSession();
         Employe utilisateur = (Employe) session.getAttribute("utilisateur");
-        if (utilisateur != null) {
+        if (utilisateur != null && (utilisateur.getService().equals("RH") || utilisateur.getFonction().equals("leader"))) {
             DemandeDAO demandeDAO = new DemandeDAO();
             // On récupère toutes les demandes ayant un état "VALIDEES" en base de données
             List<Demande> listeConges = demandeDAO.getAll().stream().filter(d -> d.getEtat() == 1).collect(Collectors.toList());
-            if (!listeConges.isEmpty()) request.setAttribute("listeConges", listeConges);
+
+            // Si la personne qui consulte la liste des congés EST UN CHEF D'EQUIPE mais pas DRH
+            if(utilisateur.getFonction().equals("leader") && !utilisateur.getService().equals("RH")) {
+                // On affiche que les congés de son équipe
+                List<Demande> listeCongesFiltrees = new ArrayList<>();
+                listeConges.forEach(c -> { // Pour chaque congé
+                    EmployeDAO employeDAO = new EmployeDAO();
+                    // Si on trouve la demande d'un membre de la même équipe que le chef d'équipe.
+                    Employe employe = employeDAO.getByEmail(c.getEmailEmploye());
+                    if(employe.getEquipe().equals(utilisateur.getEquipe())) listeCongesFiltrees.add(c);
+                });
+                listeConges = listeCongesFiltrees;
+            }
+            // S'il y a des congés validées qui n'appartiennent pas à la personne qui demande, on affiche
+            if (!listeConges.isEmpty()) request.setAttribute("listeConges",  listeConges.stream().filter(c -> !c.getEmailEmploye().equals(utilisateur.getEmail())).collect(Collectors.toList()));
+
             this.getServletContext().getRequestDispatcher(Vues.ListeDemandes.getLien()).forward(request, response);
         } else {
             response.sendRedirect("Connexion");
